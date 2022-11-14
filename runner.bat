882cb43efbba69f301418035f7b903671bb0a951
Rudresh,Tiwari <rudreshtiwari@gmail.com>
2022-11-14 11:28:28 +0530

    s

--- runner.bat
+++ runner.bat
@@ -1,3 +1,5 @@
 cd C:\elecproduvt
 behave -f allure_behave.formatter:AllureFormatter -o ./allure_reports ./features
-
+behave --junit --junit-directory jreports features/ElecrProduct.feature
+behave -f json.pretty -o json_reports features/ElecrProduct.feature
+allure serve allure_reports
